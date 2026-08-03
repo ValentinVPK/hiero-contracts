d058e61d5bbf9a6b89f4434846e94df2faf5fad3
ValentinVPK <valentin.krumov@limechain.tech>
2026-08-03 12:33:19 +0300

    chore: update forward-ports.sh for solo 0.84 namespace and pod names
    Signed-off-by: ValentinVPK <valentin.krumov@limechain.tech>

--- .github/scripts/forward-ports.sh
+++ .github/scripts/forward-ports.sh
@@ -8,22 +8,22 @@ set -euo pipefail
 # This approach keeps the connection stable and ensures it lasts throughout the tests.
 
 FORWARDS=(
-  "mirror-1-rest|5551"
-  "network-node1|50211"
-  "relay-1|7546"
-  "mirror-1-grpc|5600"
+  "mirror-ingress-controller|5551:80"
+  "network-node|50211:50211"
+  "relay-1|7546:7546"
+  "mirror-1-grpc|5600:5600"
 )
 
 ps aux | grep "port-forward" | grep kubectl | awk '{print $2}' | xargs -r kill -9
-NS="$(kubectl get ns -o name | sed 's|^namespace/||' | grep '^solo' | grep -v '^solo-setup$' | head -n1)"
+NS="$(kubectl get ns -o name | sed 's|^namespace/||' | grep 'hiero' | head -n1)"
 
 listen() {
   local pod="$1"
-  local port="$2"
+  local ports="$2"
   (
     while true; do
-      if ! ps aux | grep -F kubectl | grep -F port-forward | grep -F " ${port}:${port}" | grep -v grep >/dev/null; then
-        kubectl port-forward "$pod" -n "$NS" "${port}:${port}" >/dev/null 2>&1 &
+      if ! ps aux | grep -F kubectl | grep -F port-forward | grep -F " ${ports}" | grep -v grep >/dev/null; then
+        kubectl port-forward --address 0.0.0.0 "$pod" -n "$NS" "${ports}" >/dev/null 2>&1 &
       fi
       sleep 1
     done
@@ -31,7 +31,7 @@ listen() {
 }
 
 for row in "${FORWARDS[@]}"; do
-  IFS='|' read -r include port <<<"$row"
-  POD="$(kubectl get pods -A --no-headers | grep -E "$include" | head -n 1 | awk '{print $2}' | head -n1)"
-  listen "$POD" "$port"
+  IFS='|' read -r include ports <<<"$row"
+  POD="$(kubectl get pods -n "$NS" --no-headers | grep -E "$include" | head -n 1 | awk '{print $1}' | head -n1)"
+  listen "$POD" "$ports"
 done
